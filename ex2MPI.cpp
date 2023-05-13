b11bfba67be675278410bba22e82c2d802ce9323
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2023-05-13 16:12:41 +0300

    Update ex2MPI.cpp

--- ex2MPI.cpp
+++ ex2MPI.cpp
@@ -1,3 +1,5 @@
+
+
 #include "mpi.h"
 #include <stdio.h>
 #include <vector>
@@ -6,10 +8,10 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	cout << "0" << endl;
 	setlocale(LC_ALL, "Russian");
 	int w_size, rank, ibeg, iend, k;
 	MPI_Status stats;
+	MPI_Request reqs;
 	double startwtime = 0.0, endwtime;
 
 	const int MAX_N = 10; // максимально-допустимая размерность матрицы
@@ -18,8 +20,6 @@ int main(int argc, char** argv)
 
 	int n; // текущий размер матрицы n*n
 	srand(time(NULL));
-	cout << "1" << endl;
-
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &w_size);
@@ -29,10 +29,8 @@ int main(int argc, char** argv)
 	vector<vector<float>> a(n, vector<float>(n));
 	vector<vector<float>> b(n, vector<float>(n));
 
-	cout << "2" << endl;
 	if (rank == 0)
 	{
-		cout << "3" << endl;
 		while ((n <= 1) || (n > MAX_N))
 		{
 			n = MIN_VALUE + rand() % (MAX_VALUE - MIN_VALUE + 1);
@@ -45,17 +43,17 @@ int main(int argc, char** argv)
 			{
 				// взять случайное число
 				a[i][j] = (MIN_VALUE + rand() % (MAX_VALUE - MIN_VALUE + 1));
-				cout << a[i][j];
+				b[i][j] = a[i][j];
+				cout << a[i][j] << " ";
 			}
 			cout << endl;
 		}
-		cout << "4" << endl;
+		cout << endl;
 	}
-
 	if (w_size > n * n) w_size = n * n;
 
 	MPI_Bcast(&a, n * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
-	cout << "5" << endl;
+
 	k = n / w_size;
 	ibeg = k * rank;
 	iend = k * (rank + 1);
@@ -70,14 +68,13 @@ int main(int argc, char** argv)
 		{
 			if (i - 1 < 0 || i + 1 > n - 1 || j - 1 < 0 || j + 1 > n - 1)
 			{
-				b[i][j] = a[i][j];
+				continue;
 			}
 			else
 			{
-				b[i][j] = (a[i][j - 1] + a[i][j + 1]) / 2;
+				b[i][j] = float((a[i][j - 1] + a[i][j + 1]) / 2);
 			}
 		}
-		cout << "6" << endl;
 	}
 	if (rank == 0)
 	{
@@ -89,10 +86,10 @@ int main(int argc, char** argv)
 			}
 			cout << endl;
 		}
-		cout << "7" << endl;
 	}
 	endwtime = MPI_Wtime();
 	cout << "time is" << endwtime - startwtime << endl;
+	MPI_Waitall(w_size, &reqs, &stats);
 	MPI_Finalize();
 	return 0;
 }
